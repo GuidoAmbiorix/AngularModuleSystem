40783da6991626019b2bba99044a098d4ba06d26
Guido Amparo <GuidoA_Amparo@jce.do>
2023-01-06 07:38:37 -0400

    Working in collections

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,14 +5,17 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ]
 })
 export class CollectionsModule { }
